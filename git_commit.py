2903e1fceaafc7fcd13a8b8473caf303108dd211
Alex Foley <afgadgetboy@gmail.com>
2018-01-30 21:49:15 -0800

    ready for night 2

--- git_commit.py
+++ git_commit.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def upload():
-    subprocess.run(['git', 'add', 'output.json'], stdout=subprocess.DEVNULL)
-    subprocess.run(['git', 'commit', '-m', '"Another speed test"'], stdout=subprocess.DEVNULL)
-    subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL)
+    try:
+        subprocess.run(['git', 'add', 'output.json'], stdout=subprocess.DEVNULL)
+        subprocess.run(['git', 'commit', '-m', '"Another speed test"'], stdout=subprocess.DEVNULL)
+        subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL)
+    except:
+        # oh well, we can try again next time
