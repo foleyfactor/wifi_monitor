561d85c312534d1d873e37fef314cb90d2f52d51
Alex Foley <afgadgetboy@gmail.com>
2018-01-30 21:49:15 -0800

    ready for night 2

--- runner.py
+++ runner.py
@@ -1,12 +1,18 @@
 from download_checker import getCurrentDownloadSpeed
+from ping_checker import checkWifiStatus
 from time import sleep
 from datetime import datetime
 
 def main():
-    while datetime.now() < datetime(2018, 1, 30, 18, 27):
-        currSpeed = getCurrentDownloadSpeed()
-        print("Current download speed is: " + str(currSpeed) + "! Going to sleep...")
-        sleep(3600)
+    count = 0
+    while datetime.now() < datetime(2018, 2, 1):
+        if count == 0:
+            currSpeed = getCurrentDownloadSpeed()
+            print("Current download speed is: " + str(currSpeed) + "!")
+        res = checkWifiStatus()
+        print("Hooray! The Wifi is up!" if res else "Boo! Hiss! No Wifi. :(")
+        sleep(300)
+        count = (count + 1) % 12
 
 if __name__ == '__main__':
     main()
