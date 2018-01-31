ed177116cababa44d3849d50af5658573d8482c2
Alex Foley <afgadgetboy@gmail.com>
2018-01-30 21:51:21 -0800

    syntax error :cry:

--- git_commit.py
+++ git_commit.py
@@ -4,6 +4,6 @@ def upload():
     try:
         subprocess.run(['git', 'add', 'output.json'], stdout=subprocess.DEVNULL)
         subprocess.run(['git', 'commit', '-m', '"Another speed test"'], stdout=subprocess.DEVNULL)
-        subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL)
+        subprocess.run(['git', 'push', 'origin', 'master'], stdout=subprocess.DEVNULL)
     except:
         # oh well, we can try again next time
