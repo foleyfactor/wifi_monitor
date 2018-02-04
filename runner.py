835cb86c0e59e0af1c2f67b96cffa31864ed10cf
Alex Foley <afgadgetboy@gmail.com>
2018-02-04 00:28:26 +0000

    whoops take 2

--- runner.py
+++ runner.py
@@ -1,24 +1,24 @@
 from download_checker import getCurrentDownloadSpeed
 from ping_checker import checkWifiStatus
 from git_commit import upload, pull
-from time import sleep
+from time import sleep, time
 from datetime import datetime
 from util import getPingInterval, getDownloadInterval
 
 def main():
-    lastRunDownload = time.time() - 10000
-    lastRunPing = time.time() - 10000
+    lastRunDownload = time() - 10000
+    lastRunPing = time() - 10000
     pingInterval = getPingInterval() * 1000
     downloadInterval = getDownloadInterval() * 1000
     while True:
         pull()
 
         needUpload = False
-        if time.time() - lastRunDownload >= runInterval:
+        if time() - lastRunDownload >= runInterval:
             print("Performing Download Test")
             currSpeed = getCurrentDownloadSpeed()
             needUpload = True
-        if time.time() - lastRunDownload >= pingInterval:
+        if time() - lastRunDownload >= pingInterval:
             print("Performing Connection Test")
             res = checkWifiStatus()
             needUpload = True
